ee24d10b945c0eb49ed2ea2bda5fe6fff24afd50
Bananaha <claire.poyo@gmail.com>
2018-03-01 20:59:28 +0100

    safety commit

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const config = {
-    recast: {
-        token: 'Recast request token',
-        language: 'en',
-    },
-    smooch: {
-      keyId: 'Smooch keyId',
-      secret: 'Smooch secret',
-    },
-    port: 8080,
-}
+  recast: {
+    language: "fr",
+    token: "f2e6dc17906506b568bfee15e6226555"
+  },
+  smooch: {
+    keyId: "app_56a8b1f0809c1a2f002aee86",
+    secret: "68nWz8mE4ZZUwK0rUWrf1BTh"
+  },
+  port: 8080
+};
 
-module.exports = config
+module.exports = config;
--- src/bot.js
+++ src/bot.js
@@ -1,81 +1,94 @@
 // MODULES IMPORT
-import express from 'express'
-import bodyParser from 'body-parser'
-import request from 'superagent'
-import config from '../config'
-import jwt from 'jsonwebtoken'
-import recastai from 'recastai'
+import express from "express";
+import bodyParser from "body-parser";
+import request from "superagent";
+import config from "../config";
+import jwt from "jsonwebtoken";
+import recastai from "recastai";
 
 // POST requests Listening
-const app = express()
-const client = new recastai(config.recast.token, config.recast.language)
+const app = express();
+const client = new recastai.build(config.recast.token, config.recast.language);
 const header = {
-  alg: 'HS256',
-  typ: 'JWT',
-  kid: config.smooch.keyId,
-}
+  alg: "HS256",
+  typ: "JWT",
+  kid: config.smooch.keyId
+};
 
 const payload = {
-  scope: 'app',
-}
+  scope: "app"
+};
 
-const jwtToken = jwt.sign(JSON.stringify(payload), config.smooch.secret, { header })
+const jwtToken = jwt.sign(JSON.stringify(payload), config.smooch.secret, {
+  header
+});
 
-app.use(bodyParser.urlencoded({
-  extended: true,
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
-  handleMessage(req.body)
-  res.send('OK')
-})
+app.post("/", (req, res) => {
+  console.log("REQ BODY", req.body);
+  handleMessage(req.body);
+  res.send("OK");
+});
 
 // Message handling
-const handleMessage = (message) => {
-  const sender = message.messages[0].authorId
-  const text = message.messages[0].text
+const handleMessage = message => {
+  const sender = message.messages[0]["authorId"];
+  const text = message.messages[0]["text"];
+  console.log("tutututututututututututututu", sender, text);
 
   // CALL TO RECAST.AI: 'sender' is a unique ID of your conversation with the user
   // The conversationToken is what lets Recast.AI identify your conversation.
   // As 'sender' is what identifies your conversation with the channel used, you can use it as conversationToken.
-  client.request.converseText(text, { conversationToken: sender })
-  .then((res) => {
-    const replies = res.replies
-    const action = res.action
+  client
+    .dialog({ type: "text", content: text }, { conversationId: sender })
+    .then(res => {
+      console.log(res);
+      console.log(res.messages[0].content);
+      const reply = res.messages[0].content;
 
-    if (!replies.length) {
-      sendMessage('I didn\'t understand... Sorry :(', sender)
-      return
-    }
+      if (!reply) {
+        sendMessage("Je n'ai pas compris, désolé :(", sender);
+        return;
+      }
 
-    if (action && action.done) {
-      // Use external services: use res.memory('notion') if you got a notion from this action
-    }
+      //   const replies = res.replies;
+      //   const action = res.action;
 
-    replies.forEach(reply => sendMessage(reply, sender))
-  })
-  .catch(() => {
-    sendMessage('I need some sleep right now... Talk to me later!', sender)
-  })
-}
+      //   if (action && action.done) {
+      //     // Use external services: use res.memory('notion') if you got a notion from this action
+      //   }
+
+      sendMessage(reply, sender);
+    })
+    .catch(error => {
+      console.log(error);
+      sendMessage("I need some sleep right now... Talk to me later!", sender);
+    });
+};
 
 // Message sending
 const sendMessage = (text, recipient) => {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
     const message = {
-      role: 'appMaker',
-      text,
-    }
-    request.post(`https://api.smooch.io/v1/appusers/${recipient}/messages`)
-    .send(message)
-    .set('content-type', 'application/json')
-    .set('authorization', `Bearer ${jwtToken}`)
-    .end(() => {
-      return resolve()
-    })
-  })
-}
+      role: "appMaker",
+      text
+    };
+    request
+      .post(`https://api.smooch.io/v1/appusers/${recipient}/messages`)
+      .send(message)
+      .set("content-type", "application/json")
+      .set("authorization", `Bearer ${jwtToken}`)
+      .end(() => {
+        return resolve();
+      });
+  });
+};
 
-app.listen(config.port)
+app.listen(config.port);
